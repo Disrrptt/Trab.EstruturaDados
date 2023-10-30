23d4b0f4993b9fdce578f7b9109312fb1b22b285
Luiza Munis <luizamunisnascimento@gmail.com>
2023-10-30 20:50:57 -0300

    atualizaçaõ 3

--- matriz.c
+++ matriz.c
@@ -1,1 +1,20 @@
 //implementação de funções
+
+#include <stdio.h>
+#include <stdlib.h>
+ 
+
+ //função para ler arquivo
+
+void leMatriz(const char *Arquivo,double valor, int *linhas, int *colunas){
+
+FILE *file;
+    
+    file = fopen("DadosEntradaMatriz.txt", "r");
+    //verifica se abre o arquivo e retorna
+    if (file == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return 0;
+    }
+
+}
