322bf7050cdc9efc7ea1aeea0943c64555440d3f
Luiza Munis <luizamunisnascimento@gmail.com>
2023-10-30 20:25:37 -0300

    atualização 2

--- matriz.h
+++ matriz.h
@@ -1,1 +1,11 @@
 // cabeçalhos e structs
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Estrutura para armazenar um elemento da matriz
+typedef struct {
+    int i;     // Linha
+    int j;     // Coluna
+    double valor;  // Valor do elemento
+} ElementoMatriz; 
